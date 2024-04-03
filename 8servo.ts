a853295395f40bc7a59697bd978a41116160edad
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2024-04-04 08:39:23 +0900

    更新 8servo.ts, test.ts

--- 8servo.ts
+++ 8servo.ts
@@ -1,13 +1,10 @@
 /**
- * 8angle potentio meter
+ * 8servo servo motor driver
  */
-//% weight=10 color=#800080 icon="\u261d" block="8angle"
-namespace angle8 {
-    let I2C_ADDR = 0x43
+//% weight=10 color=#800080 icon="\u261d" block="8servo"
+namespace servo8 {
+    let I2C_ADDR = 0x25
 
-    /**
-     * set reg
-     */
     function setReg8(reg: number, dat: number): void {
         let buf = pins.createBuffer(2);
         buf[0] = reg;
@@ -21,6 +18,14 @@ namespace angle8 {
         buf[2] = dat >> 8;
         pins.i2cWriteBuffer(I2C_ADDR, buf);
     }
+    function setReg24(reg: number, dat: number[]): void {
+        let buf = pins.createBuffer(4);
+        buf[0] = reg;
+        buf[1] = dat[0];
+        buf[2] = dat[1];
+        buf[3] = dat[2];
+        pins.i2cWriteBuffer(I2C_ADDR, buf);
+    }
     function setReg32(reg: number, dat: number[]): void {
         let buf = pins.createBuffer(5);
         buf[0] = reg;
@@ -30,9 +35,6 @@ namespace angle8 {
         buf[4] = dat[3];
         pins.i2cWriteBuffer(I2C_ADDR, buf);
     }
-    /**
-     * get reg
-     */
     function getReg8(reg: number): number {
         pins.i2cWriteNumber(I2C_ADDR, reg, NumberFormat.UInt8BE);
         return pins.i2cReadNumber(I2C_ADDR, NumberFormat.UInt8BE);
@@ -41,62 +43,65 @@ namespace angle8 {
         pins.i2cWriteNumber(I2C_ADDR, reg, NumberFormat.UInt8BE);
         return pins.i2cReadNumber(I2C_ADDR, NumberFormat.UInt16LE);
     }
+    function getReg32(reg: number): number {
+        pins.i2cWriteNumber(I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(I2C_ADDR, NumberFormat.UInt32LE);
+    }
+    function getReg32f(reg: number): number {
+        pins.i2cWriteNumber(I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(I2C_ADDR, NumberFormat.Float32LE);
+    }
 
-    /**
-     * input8
-     * @param ch number,eg:0
-     */
+    //% blockId="modeSet" block="modeSet ch:%ch mode:%m"
+    export function modeSet(ch: number,m:number): void {
+        setReg8(ch,m);
+        return;
+    }
+    //% blockId="outputD" block="outputD ch:%ch CTRL:%c"
+    export function outputD(ch: number, c: number): void {
+        setReg8(0x10 + ch, c);
+        return;
+    }
+    //% blockId="inputD" block="inputD ch:%ch"
+    export function inputD(ch: number): number {
+        return getReg8(0x20 + ch);
+    }
     //% blockId="input8" block="input8 ch:%ch"
-    //% weight=90 blockGap=8
     export function input8(ch: number): number {
-        return getReg8(0x10 + ch);
+        return getReg8(0x30 + ch);
     }
-    /**
-     * input12
-     * @param ch number,eg:0
-     */
     //% blockId="input12" block="input12 ch:%ch"
-    //% weight=90 blockGap=8
     export function input12(ch: number): number {
-        return getReg16(ch);
+        return getReg16(0x40 + ch * 2);
     }
-    /**
-     * inputSW
-     */
-    //% blockId="inputSW" block="inputSW"
-    //% weight=90 blockGap=8
-    export function inputSW(): number {
-        return getReg8(0x20);
+    //% blockId="servo8" block="servo8 ch:%ch degree:%d"
+    export function servo8(ch: number, d: number): void {
+        setReg8(0x50 + ch, d);
+        return;
     }
-    /**
-     * setRGB
-     */
-    //% blockId="setRGB" block="setRGB led:%led red:%r green:%g blue:%b bright:%br"
-    export function setRGB(led: number, r: number, g: number, b: number, br: number): void {
-        let dat = [0, 0, 0, 0];
+    //% blockId="servo16" block="servo16 ch:%ch microSecond:%us"
+    export function servo16(ch: number, us: number): void {
+        setReg8(0x60 + ch * 2, us);
+        return;
+    }
+    //% blockId="setRGB" block="setRGB led:%led red:%r green:%g blue:%b"
+    export function setRGB(led: number, r: number, g: number, b: number): void {
+        let dat = [0, 0, 0];
         dat[0] = r;
         dat[1] = g;
         dat[2] = b;
-        dat[3] = br;
-        setReg32(0x30 + led * 4, dat);
+        setReg24(0x70 + led * 3, dat);
         return;
     }
-    /**
-     * setRGB
-     */
-    //% blockId="setColor" block="setColor led:%led color:%c bright:%br"
-    export function setColor(led: number, c: number, br: number): void {
-        let dat = [0, 0, 0, 0];
+    //% blockId="setColor" block="setColor led:%led color:%c"
+    export function setColor(led: number, c: number): void {
+        let dat = [0, 0, 0];
         dat[0] = c >> 16;
         dat[1] = c >> 8 & 0xff;
         dat[2] = c & 0xff;
-        dat[3] = br;
-        setReg32(0x30 + led * 4, dat);
+        setReg24(0x70 + led * 3, dat);
         return;
     }
-    /**
-     * setRGB
-     */
     //% blockId="getColor" block="getColor red:%r green:%g blue:%b"
     export function getColor(r: number, g: number, b: number): number {
         return (r * 256 + g) * 256 + b;
--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+for (let ch = 0; ch < 8; ch++) {
+    servo8.modeSet(ch,3);   // set to servo mode
+}
+basic.forever(function() {
+    for(let deg = 0;deg <=180;deg+=10){
+        for(let ch = 0;ch <8;ch++){
+            servo8.servo8(ch,deg);
+            basic.pause(200);
+        }
+        basic.pause(1000);
+    }
+})
