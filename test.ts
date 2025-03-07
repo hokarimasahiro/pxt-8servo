f0e3b45f7fd7c7a56d627a3f049e79ffe8293266
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-03-08 03:42:48 +0900

    更新 pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -5,8 +5,8 @@ basic.forever(function() {
     for(let deg = 0;deg <=180;deg+=10){
         for(let ch = 0;ch <8;ch++){
             servo8.servo8(ch,deg);
-            basic.pause(200);
+            basic.pause(100);
         }
-        basic.pause(1000);
+        basic.pause(500);
     }
 })
